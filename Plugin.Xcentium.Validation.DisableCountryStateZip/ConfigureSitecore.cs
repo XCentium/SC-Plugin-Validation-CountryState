543610737ed2eb24c29211d76aa8273041cc48a3
olaowolawi <ola.owolawi@xcentium.com>
2017-07-03 13:24:41 -0500

    Initial Commit

---
+++ Plugin.Xcentium.Validation.DisableCountryStateZip/ConfigureSitecore.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Reflection;
+
+using Microsoft.Extensions.DependencyInjection;
+using Plugin.Xcentium.Validation.DisableCountryStateZip.Pipelines.Blocks;
+using Sitecore.Commerce.Core;
+using Sitecore.Commerce.EntityViews;
+using Sitecore.Commerce.Plugin.Carts;
+using Sitecore.Commerce.Plugin.Fulfillment;
+using Sitecore.Framework.Configuration;
+using Sitecore.Framework.Pipelines;
+using Sitecore.Framework.Pipelines.Definitions.Extensions;
+
+namespace Plugin.Xcentium.Validation.DisableCountryStateZip
+{
+    /// <summary>
+    /// The Mayo configure class.
+    /// </summary>
+    /// <seealso cref="Sitecore.Framework.Configuration.IConfigureSitecore" />
+    public class ConfigureSitecore : IConfigureSitecore
+    {
+
+        /// <summary>The configure services.</summary>
+        /// <param name="services">The services.</param>
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            services.RegisterAllPipelineBlocks(assembly);
+
+            Action<SitecorePipelinesConfigBuilder> actionDelegate = c => c
+                .ConfigurePipeline<IValidatePartyPipeline>(
+                    d =>
+                    {
+                        d.Replace<ValidatePartyBlock, DisableContryStateZipValidationBlock>();
+                    });
+
+            services.Sitecore().Pipelines(actionDelegate);
+
+        }
+
+
+    }
+
+
+}
---
+++ Plugin.Xcentium.Validation.DisableCountryStateZip/Pipelines/Blocks/DisableContryStateZipValidationBlock.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Sitecore.Commerce.Core;
+using Sitecore.Framework.Conditions;
+using Sitecore.Framework.Pipelines;
+
+namespace Plugin.Xcentium.Validation.DisableCountryStateZip.Pipelines.Blocks
+{
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public class DisableContryStateZipValidationBlock : PipelineBlock<Party, Party, CommercePipelineExecutionContext>
+    {
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public DisableContryStateZipValidationBlock() : base((string)null)
+        {
+            
+        }
+
+        /// <summary>
+        /// Task
+        /// </summary>
+        /// <param name="arg"></param>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public override async Task<Party> Run(Party arg, CommercePipelineExecutionContext context)
+        {
+            Condition.Requires<Party>(arg).IsNotNull<Party>(string.Format("{0}: The argument cannot be null.", (object)this.Name));
+            var party = this.ValidateParty(arg, context);
+            if (party == null)
+                context.CommerceContext.AddMessage(context.GetPolicy<KnownResultCodes>().Error, "AddressNotValid", new object[1]
+                {
+          (object) arg.AddressName
+                }, string.Format("Address {0} is not valid.", (object)arg.AddressName));
+            await Task.Delay(100);
+            return party;
+        }
+
+        private Party ValidateParty(Party party, CommercePipelineExecutionContext context)
+        {
+            Condition.Requires<Party>(party).IsNotNull<Party>(string.Format("{0}: The party can not be null", (object)this.Name));
+            var success = true;
+            success = this.ValidateProperty("AddressName", party.AddressName, context);
+            success &= this.ValidateProperty("Address1", party.Address1, context);
+            success &= this.ValidateProperty("City", party.City, context);
+            return !success ? null : party;
+        }
+
+        private bool ValidateProperty(string propertyName, string propertyValue, CommercePipelineExecutionContext context)
+        {
+            Condition.Requires<string>(propertyName).IsNotNull<string>(string.Format("{0}: The propertyName can not be null", (object)this.Name));
+            if (!string.IsNullOrEmpty(propertyValue))
+                return true;
+            context.CommerceContext.AddMessage(context.GetPolicy<KnownResultCodes>().Error, "InvalidOrMissingPropertyValue", new object[1]
+            {
+        (object) propertyName
+            }, string.Format("Invalid or missing value for property ' {0}'.", (object)propertyName));
+            return false;
+        }
+
+
+
+    }
+}

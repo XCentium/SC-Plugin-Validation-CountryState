a74e39c7c7889ae969a80359d3a0b8e0edf1353c
ola owolawi <ola.owolawi@xcentium.com>
2018-02-05 16:52:41 -0800

    SXC9Initial Upgrade

--- Plugin.Xcentium.Validation.DisableCountryStateZip/Pipelines/Blocks/DisableContryStateZipValidationBlock.cs
+++ Plugin.Xcentium.Validation.DisableCountryStateZip/Pipelines/Blocks/DisableContryStateZipValidationBlock.cs
@@ -31,36 +31,37 @@ namespace Plugin.Xcentium.Validation.DisableCountryStateZip.Pipelines.Blocks
         /// <returns></returns>
         public override async Task<Party> Run(Party arg, CommercePipelineExecutionContext context)
         {
-            Condition.Requires<Party>(arg).IsNotNull<Party>(string.Format("{0}: The argument cannot be null.", (object)this.Name));
+            Condition.Requires<Party>(arg).IsNotNull<Party>($"{(object) this.Name}: The argument cannot be null.");
             var party = this.ValidateParty(arg, context);
             if (party == null)
-                context.CommerceContext.AddMessage(context.GetPolicy<KnownResultCodes>().Error, "AddressNotValid", new object[1]
-                {
-          (object) arg.AddressName
-                }, string.Format("Address {0} is not valid.", (object)arg.AddressName));
-            await Task.Delay(100);
+                await context.CommerceContext.AddMessage(context.GetPolicy<KnownResultCodes>().Error, "AddressNotValid",
+                    new object[1]
+                    {
+                        (object) arg.AddressName
+                    }, $"Address {(object) arg.AddressName} is not valid.");
             return party;
         }
 
         private Party ValidateParty(Party party, CommercePipelineExecutionContext context)
         {
-            Condition.Requires<Party>(party).IsNotNull<Party>(string.Format("{0}: The party can not be null", (object)this.Name));
+            Condition.Requires<Party>(party).IsNotNull<Party>($"{(object) this.Name}: The party can not be null");
             var success = true;
-            success = this.ValidateProperty("AddressName", party.AddressName, context);
-            success &= this.ValidateProperty("Address1", party.Address1, context);
-            success &= this.ValidateProperty("City", party.City, context);
+            success = this.ValidateProperty("AddressName", party.AddressName, context).Result;
+            success &= this.ValidateProperty("Address1", party.Address1, context).Result;
+            success &= this.ValidateProperty("City", party.City, context).Result;
             return !success ? null : party;
         }
 
-        private bool ValidateProperty(string propertyName, string propertyValue, CommercePipelineExecutionContext context)
+        private async Task<bool> ValidateProperty(string propertyName, string propertyValue, CommercePipelineExecutionContext context)
         {
-            Condition.Requires<string>(propertyName).IsNotNull<string>(string.Format("{0}: The propertyName can not be null", (object)this.Name));
+            Condition.Requires<string>(propertyName).IsNotNull<string>(
+                $"{(object) this.Name}: The propertyName can not be null");
             if (!string.IsNullOrEmpty(propertyValue))
                 return true;
-            context.CommerceContext.AddMessage(context.GetPolicy<KnownResultCodes>().Error, "InvalidOrMissingPropertyValue", new object[1]
+           await context.CommerceContext.AddMessage(context.GetPolicy<KnownResultCodes>().Error, "InvalidOrMissingPropertyValue", new object[1]
             {
         (object) propertyName
-            }, string.Format("Invalid or missing value for property ' {0}'.", (object)propertyName));
+            }, $"Invalid or missing value for property ' {(object) propertyName}'.");
             return false;
         }
 
